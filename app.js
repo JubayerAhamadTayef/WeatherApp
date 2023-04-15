5a04b545e3e31af6c8d52d12896612f6228c794a
Jubayer Ahamad Tayef <129745706+JubayerAhamadTayef@users.noreply.github.com>
2023-04-15 09:29:21 +0600

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const apiKey=`27887ba926cfd4c2f4257df171d0010f`;
 const searchTemperature=()=>{
 const cityname=document.getElementById("city").value;
-const url=`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}&unit=metric`;
+const url=`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}&units=metric`;
 fetch(url)
 .then(res=>res.json())
 .then(data=>displayTemparature(data))
@@ -11,6 +11,7 @@ document.getElementById(id).innerText=text;
 }
 const displayTemparature=temparature=>{
 setInnerText('city-name',temparature.name);
+setInnerText('time',temparature.timezone);
 setInnerText('temperature',temparature.main.temp);
 setInnerText('condition',temparature.weather[0].main);
 setInnerText('situation',temparature.weather[0].description);
@@ -18,6 +19,8 @@ setInnerText('feels',temparature.main.feels_like);
 setInnerText('max',temparature.main.temp_max);
 setInnerText('min',temparature.main.temp_min);
 setInnerText('country',temparature.sys.country);
+setInnerText('sunrise',temparature.sys.sunrise);
+setInnerText('sunset',temparature.sys.sunset);
 const iconUrl=`https://openweathermap.org/img/wn/${temparature.weather[0].icon}@2x.png`;
 const imgIcon=document.getElementById("weather-ico")
 imgIcon.setAttribute('src',iconUrl)
